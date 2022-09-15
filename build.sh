7e61382d29ca8dab6803f948008450a435631377
ruineka <ruineka@pop-os.localdomain>
2022-09-14 18:48:12 -0600

    Don't reinstall kernel a second time.

--- build.sh
+++ build.sh
@@ -138,7 +138,7 @@ pacman --noconfirm -U https://github.com/ruineka/linux/releases/download/v6.0-rc
 pacman --noconfirm -U https://github.com/ruineka/mesa-22.3.0/releases/download/mesa-intel-gamescope/mesa-git-22.3.0_devel.158154.33e81ff0fba.d41d8cd98f00b204e9800998ecf8427e-1-x86_64.pkg.tar.zst
 pacman --noconfirm -U https://github.com/ruineka/mesa-22.3.0/releases/download/mesa-intel-gamescope/lib32-mesa-git-22.3.0_devel.158154.33e81ff0fba.d41d8cd98f00b204e9800998ecf8427e-1-x86_64.pkg.tar.zst
 
-pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
+#pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
 
 # install packages
 pacman --noconfirm -S --overwrite '*' ${PACKAGES}
