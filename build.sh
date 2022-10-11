ee750237ea16699f62010ec8bdb2d7f360b4750f
Matthew Anderson <ruinairas1992@gmail.com>
2022-10-10 21:14:55 -0500

    Remove GE's GLIBC (BREAKS), Fixed an issue with Mesa.

--- build.sh
+++ build.sh
@@ -138,16 +138,9 @@ pacman --noconfirm -U https://github.com/ruineka/linux/releases/download/v5.19.1
 pacman --noconfirm -U https://github.com/ruineka/linux/releases/download/v5.19.13-Ruineka/linux-ruineka-docs-5.19.13-1-x86_64.pkg.tar.zst
 
 # install custom patched mesa for Intel
-pacman --noconfirm -U https://github.com/ruineka/mesa-22.3.0/releases/download/mesa-intel-updated-gamescope/mesa-git-ruineka-22.3.0_devel.158155.9aea4efcb94.d41d8cd98f00b204e9800998ecf8427e-1-x86_64.pkg.tar.zst
+pacman --noconfirm -U https://github.com/ruineka/mesa-22.3.0/releases/download/mesa-intel-updated-gamescope/mesa-git-22.3.0_devel.158155.9aea4efcb94.d41d8cd98f00b204e9800998ecf8427e-1-x86_64.pkg.tar.zst
 
-pacman --noconfirm -U https://github.com/ruineka/mesa-22.3.0/releases/download/mesa-intel-updated-gamescope/lib32-mesa-git-ruineka-22.3.0_devel.158155.9aea4efcb94.d41d8cd98f00b204e9800998ecf8427e-1-x86_64.pkg.tar.zst
-
-# Install GE's patched glibc to enable rogue company and other EAC games broken with latest glibc changes.
-pacman --noconfirm -U https://github.com/ruineka/glibc-eac-rc/raw/main/compiled-packages/glibc-2.35-6-x86_64.pkg.tar.zst
-
-pacman --noconfirm -U https://github.com/ruineka/glibc-eac-rc/raw/main/compiled-packages/glibc-debug-2.35-6-x86_64.pkg.tar.zst
-
-pacman --noconfirm -U https://github.com/ruineka/glibc-eac-rc/raw/main/compiled-packages/lib32-glibc-2.35-6-x86_64.pkg.tar.zst
+pacman --noconfirm -U https://github.com/ruineka/mesa-22.3.0/releases/download/mesa-intel-updated-gamescope/lib32-mesa-git-22.3.0_devel.158155.9aea4efcb94.d41d8cd98f00b204e9800998ecf8427e-1-x86_64.pkg.tar.zst
 
 #pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
 
